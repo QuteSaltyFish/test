f10f4ce191159364b2027b41cd221ae88509f318
Mingke Wang <qutesaltyfish@Mingkes-MacBook-Pro.local>
2021-06-10 21:17:40 +0800

    update

--- test.py
+++ test.py
@@ -1,2 +1,29 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
+import pyautogui
+import time
+# %%
+# pyautogui.mouseInfo()
+
+# %%
+time.sleep(10)
+for i in range(10):
+    pyautogui.hscroll(-10)
+    time.sleep(3)
+
+# %%
+time.sleep(10)
+for i in range(10):
+    pyautogui.hscroll(-10)
+    time.sleep(3)
+    pyautogui.hscroll(10)
+    time.sleep(3)
+
+# %%
+pyautogui.size()
+# %%
+pyautogui.position()
+# %%
+pyautogui.hscroll(-10)
+# %%
