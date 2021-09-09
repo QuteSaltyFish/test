86bce1e2fe6f417f61edfe987321afaf9d7b8ae6
mingkew <mingkew@umich.edu>
2021-09-09 16:43:28 -0400

    s

---
+++ Julia/test.jl
@@ -0,0 +1,3 @@
+using Pandas, PyPlot
+df = DataFrame(Dict(:age=>[27, 29, 27], :name=>["James", "Jill", "Jake"]))
+describe(df)
--- test.py
+++ test.py
@@ -5,7 +5,15 @@ import pyautogui
 import time
 # %%
 # pyautogui.mouseInfo()
+computer = np.array([
+    59.99, 10.99*3, 66.99, 49.99, 54.99,
+])
+print(np.sum(computer))
 
+others = np.array([
+    5.83, 239.99, 107.99, 152.35, 59.99, 139.99, 53.99
+])
+print(np.sum(others))
 # %%
 time.sleep(10)
 for i in range(10):
